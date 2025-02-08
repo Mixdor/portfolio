c7f8bf91c4dc8101a9ec10fb7c24ff716732cd4f
mixdor <miguel-avila112-@outlook.com>
2025-02-07 21:00:08 -0500

    Configured astro for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://Mixdor.github.io',
+  base: '/circuitikz-generator.github.io',
+})
